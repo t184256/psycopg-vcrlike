fba1957d0f754810bffcec32016839a1fdbfe996
Alexander Sosedkin <asosedkin@redhat.com>
2023-11-13 18:25:02 +0100

    test_http: scrub non-deterministic data from the response

--- tests/test_http.py
+++ tests/test_http.py
@@ -3,6 +3,8 @@
 
 """Test that the plugin doesn't break regular aiohttp recording."""
 
+import typing
+
 import aiohttp
 import pytest
 
@@ -14,3 +16,29 @@ async def test_smoke_aiohttp() -> None:
     async with aiohttp.ClientSession() as sess, sess.get(url) as response:
         html = await response.text()
         assert '<title>Example Domain</title>' in html
+
+
+# pytest-recording configuration
+
+
+_SCRUB_HEADERS_RESPONSE = ('Age', 'Date', 'Etag', 'Expires', 'Server')
+
+
+def _scrub_response(
+    response: dict[str, dict[str, bytes]],
+) -> dict[str, dict[str, bytes]]:
+    headers = response['headers'].copy()
+    for h in _SCRUB_HEADERS_RESPONSE:
+        if h in headers:
+            del headers[h]
+    response['headers'] = headers
+    return response
+
+
+@pytest.fixture(scope='session', autouse=True)
+def vcr_config() -> dict[str, typing.Any]:
+    """Filter out sensitive info from cassettes."""
+    return {
+        'before_record_response': _scrub_response,
+        'decode_compressed_response': True,
+    }
